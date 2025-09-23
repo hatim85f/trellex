6c0ba5d05332851a6565f5c9fe35529ddc52f997
hatim85f <50639901+hatim85f@users.noreply.github.com>
2025-09-23 22:48:27 +0400

    solved email issue

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -28,27 +28,4 @@ router.put("/:notificationId", async (req, res) => {
   }
 });
 
-router.post("/mail", async (req, res) => {
-  const { email, name } = req.body;
-
-  try {
-    const code = Math.floor(100000 + Math.random() * 900000);
-
-    await sendTemplateEmail({
-      to: email,
-      name: name,
-      templateId: 1,
-      params: {
-        full_name: name,
-        code: `${code}`,
-        time_create: moment(new Date()).format("hh:mm A MMMM Do, YYYY"),
-      },
-    });
-
-    return res.status(200).send({ message: "Email Sent Successfully" });
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
-  }
-});
-
 module.exports = router;
