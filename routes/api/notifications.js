2e5749c8b2e721aa297753f40f7a5286ed33d860
hatim85f <50639901+hatim85f@users.noreply.github.com>
2025-10-16 01:29:20 +0400

    testing notifications

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -4,6 +4,53 @@ const Notification = require("../../models/Notification");
 const { sendTemplateEmail } = require("../../lib/brevo");
 
 const moment = require("moment");
+const sendNotification = require("../../helpers/sendNotification");
+const User = require("../../models/User");
+
+router.get("/", async (req, res) => {
+  res.status(200).send("Notifications route is working");
+});
+
+router.post("/notifications", async (req, res) => {
+  const { userIds, from, title, subject, message } = req.body;
+
+  try {
+    const users = await User.find({ _id: "68c48acfd6066a3739538cf8" });
+
+    let pushTokens = [];
+    for (let user of users) {
+      if (user.pushTokens) {
+        pushTokens.push(...user.pushTokens);
+      }
+    }
+
+    // Send push notifications
+    if (pushTokens.length > 0) {
+      await sendNotification({
+        from,
+        title,
+        subject,
+        pushTokens,
+      });
+    }
+
+    userIds.map((a) => {
+      const newNotification = new Notification({
+        to: a,
+        from,
+        title,
+        subject,
+        message,
+      });
+      newNotification.save();
+    });
+
+    return res.status(200).send({ message: "Notification saved successfully" });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Server error" });
+  }
+});
 
 router.put("/:notificationId", async (req, res) => {
   const { notificationId } = req.params;
